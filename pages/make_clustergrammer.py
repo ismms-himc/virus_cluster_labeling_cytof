fed92a37ce90ee8f18473ef0be23950ad86384e4
cornhundred <nickfloresfernandez@gmail.com>
2017-07-18 17:57:00 -0400

    made original and predicted category colors the same (only effects Chik virus groups)

--- pages/make_clustergrammer.py
+++ pages/make_clustergrammer.py
@@ -11,33 +11,21 @@ from clustergrammer import Network
 net = Network()
 
 # load matrix tsv file
-net.load_file('txt/rc_two_cats.txt')
-# net.load_file('txt/ccle_example.txt')
-# net.load_file('txt/rc_val_cats.txt')
-# net.load_file('txt/number_labels.txt')
-# net.load_file('txt/mnist.txt')
-# net.load_file('txt/tuple_cats.txt')
-# net.load_file('txt/example_tsv.txt')
-
-# net.enrichrgram('KEA_2015')
-
-# optional filtering and normalization
-##########################################
-# net.filter_sum('row', threshold=20)
-# net.normalize(axis='col', norm_type='zscore', keep_orig=True)
-# net.filter_N_top('row', 250, rank_type='sum')
-# net.filter_threshold('row', threshold=3.0, num_occur=4)
-# net.swap_nan_for_zero()
-# net.set_cat_color('col', 1, 'Category: one', 'blue')
-
-  # net.make_clust()
-  # net.dendro_cats('row', 5)
-
-# net.cluster(dist_type='cos',views=['N_row_sum', 'N_row_var'] , dendro=True,
-#                sim_mat=True, filter_sim=0.1, calc_cat_pval=False, enrichrgram=False)
+net.load_file('../data_mats/df_predict_merge.txt')
 
 net.set_cat_color('row', 1, 'virus: chik', 'blue')
 net.set_cat_color('row', 1, 'virus: zika', 'red')
+net.cluster(enrichrgram=False)
+
+# transfer colors from original to predicted categories
+########################################################
+
+# make category colors the same for Chik groups
+for inst_cat in net.viz['cat_colors']['row']['cat-1']:
+    new_cat = inst_cat.replace('original','predict')
+    inst_color = net.viz['cat_colors']['row']['cat-1'][inst_cat]
+    net.set_cat_color('row', 3, new_cat, inst_color)
+
 net.cluster(enrichrgram=False)
 
 # write jsons for front-end visualizations
